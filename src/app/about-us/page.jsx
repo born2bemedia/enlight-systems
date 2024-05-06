6084b49f3df02be40f48d9a8c726d81794cf8654
SergWebAcceptance <serg.web@acceptance.ltd>
2024-05-06 23:03:22 +0300

    Added meta to about us

--- src/app/about-us/page.jsx
+++ src/app/about-us/page.jsx
@@ -6,6 +6,16 @@ import AboutLast from "./_components/AboutLast";
 import AboutSecond from "./_components/AboutSecond";
 import AboutThird from "./_components/AboutThird";
 
+export const metadata = {
+  title: "About Us",
+  description: "Meet Enlight Systems! Learn how we came to design Enlight Dashboard, what challenges, problems, and issues the dashboard covers, and how we will optimize it!",
+  openGraph: {
+    title: "About Us",
+    description: "Meet Enlight Systems! Learn how we came to design Enlight Dashboard, what challenges, problems, and issues the dashboard covers, and how we will optimize it!",
+    images: "https://enlight.systems/images/meta.png",
+  },
+};
+
 function AboutUs() {
   return (
     <>
