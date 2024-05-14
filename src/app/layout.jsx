647e6513110ceb9fb92f0f7c58fb27ed0b5d4493
SergWebAcceptance <serg.web@acceptance.ltd>
2024-05-14 11:15:09 +0300

    Added analytics

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,6 +3,7 @@ import "@/public/scss/base.scss";
 import Header from "../component/Header";
 import Footer from "../component/Footer";
 import Preloader from "../component/Preloader";
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 const epilogue = Epilogue({ subsets: ["latin"] });
 
@@ -28,6 +29,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={epilogue.className}>
+        <GoogleAnalytics gaId="G-N1SFZ1RDHK" />
         <Preloader/>
         <Header />
         <main>{children}</main>
